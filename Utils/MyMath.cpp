13d1ed7171402f04349f4440f31ae7c0d5325520
Benoît Ruiz <bruizy@gmail.com>
2017-10-12 18:50:40 +0200

    add a reflect function to Vector3d

--- Utils/MyMath.cpp
+++ Utils/MyMath.cpp
@@ -1,5 +1,14 @@
 #include "MyMath.h"
 
+namespace Eigen
+{
+    Vector3d reflect(const Vector3d& v1, const Vector3d& v2)
+    {
+        return v1 - 2 * v2 * v1.dot(v2);
+    }
+
+}
+
 namespace std
 {
     double clamp(double v, double min, double max) { return v < max ? v < min ? min : v : max; }
--- Utils/MyMath.h
+++ Utils/MyMath.h
@@ -1,7 +1,17 @@
 #ifndef MYMATH_H
 #define MYMATH_H
 
+#include <Eigen/Dense>
+using namespace Eigen;
+
 static constexpr double M_PI = 3.141592653589793238462643383279;
+
+namespace Eigen
+{
+
+    Vector3d reflect(const Vector3d& v1, const Vector3d& v2);
+
+}
 
 namespace std
 {
