e1301733737089cc14dee1a2cbc1d6e8c06cc154
Benoît Ruiz <bruizy@gmail.com>
2017-10-12 14:00:08 +0200

    removed codeblock files so gitignore works properly. Added Eigen library

--- main.cpp
+++ main.cpp
@@ -2,10 +2,22 @@
 
 #include "Scenes/BasicScene.h"
 
+#include <Eigen/Core>
+
+using Eigen::MatrixXd;
+
 int main()
 {
+
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
+
     BasicScene scene;
 
-    scene.render("img2");
+    //scene.render("img2");
     return 0;
 }
