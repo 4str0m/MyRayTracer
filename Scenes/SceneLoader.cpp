ae14e68070e4313d26302c516750ebd36d61aded
Benoît Ruiz <bruizy@gmail.com>
2017-10-26 21:04:07 +0200

    Update sceneLoader, not finished yet

--- Scenes/SceneLoader.cpp
+++ Scenes/SceneLoader.cpp
@@ -2,8 +2,9 @@
 
 #include <iostream>
 #include <fstream>
-
 #include <regex>
+
+#include "../Utils/Converter.h"
 
 bool SceneLoader::loadSceneFromFile(const std::string& fileName, Scene &scene)
 {
@@ -15,91 +16,82 @@ bool SceneLoader::loadSceneFromFile(const std::string& fileName, Scene &scene)
         return false;
     }
     std::string line;
-    std::regex fileNameRegex("fileName\\s+(.*)");
-    std::regex cameraRegex("camera\\s+(.*)");
-    std::regex materialRegex("material\\s+(.*)");
-    std::regex lightRegex("light\\s+(.*)");
-    std::regex sphereRegex("sphere\\s+(.*)");
+    std::regex fileNameRegex("^fileName\\s+");
+    std::regex cameraRegex("^camera\\s+");
+    std::regex materialRegex("^material\\s+");
+    std::regex lightRegex("^light\\s+");
+    std::regex sphereRegex("^sphere\\s+");
 
     while(getline(file, line))
     {
         std::smatch matches;
-        if (std::regex_match(line, matches, fileNameRegex))
+        if (std::regex_search(line, matches, fileNameRegex))
         {
 
         }
-        else if (std::regex_match(line, matches, cameraRegex))
+        else if (std::regex_search(line, matches, cameraRegex))
         {
-            std::cout << "camera" << std::endl;
-            if(!loadCameraFromString(matches[1].str(), scene))
-            {
-                return false;
-            }
+            std::cout << matches[0].str() << matches.suffix() << std::endl;
+
         }
-        else if (std::regex_match(line, matches, materialRegex))
+        else if (std::regex_search(line, matches, materialRegex))
         {
-            std::cout << "material" << std::endl;
-            if(!loadMaterialFromString(matches[1].str(), scene))
-            {
-                return false;
-            }
+            std::cout << matches[0].str() << matches.suffix() << std::endl;
         }
-        else if (std::regex_match(line, matches, lightRegex))
+        else if (std::regex_search(line, matches, lightRegex))
         {
-            std::cout << "light" << std::endl;
-            if(!loadLightFromString(matches[1].str(), scene))
-            {
-                return false;
-            }
+            std::cout << matches[0].str() << matches.suffix() << std::endl;
         }
-        else if (std::regex_match(line, matches, sphereRegex))
+        else if (std::regex_search(line, matches, sphereRegex))
         {
-            std::cout << "sphere" << std::endl;
-            if(!loadSphereFromString(matches[1].str(), scene))
-            {
-                return false;
-            }
+            std::cout << matches[0].str() << matches.suffix() << std::endl;
         }
     }
     file.close();
-    return true;
+    return false;
 }
 
-bool SceneLoader::loadCameraFromString (const std::string& line, Scene &scene)
+bool SceneLoader::loadNameFromString (const std::string& line, std::string &name)
 {
-    // Camera: width height pos focalLength
-    std::regex cameraRegex("(\\d+)\\s+(\\d+)\\s+\\((.+)\\)\\s+(\\d+\\.?\\d*)");
-    return false;
-}
-bool SceneLoader::loadMaterialFromString (const std::string& line, Scene &scene)
-{
-    // Material: materialName radius origin
-    std::regex materialRegex("([a-zA-Z0-9_]+)\\s+\\((.+)\\)\\s+\\((.+)\\)\\s+\\((.+)\\)\\s+(\\d+\\.?\\d*)");
-    return false;
-}
-bool SceneLoader::loadLightFromString (const std::string& line, Scene &scene)
-{
-    // Light: pos color power
-    std::regex lightRegex("\\((.+)\\)\\s+\\((.+)\\)\\s+(\\d+\\.?\\d*)");
-    return false;
-}
-bool SceneLoader::loadSphereFromString (const std::string& line, Scene &scene)
-{
-    // Sphere: materialName radius origin
-    std::regex sphereRegex("([a-zA-Z0-9_]+)\\s+(\\d+\\.?\\d*)\\s+\\((.+)\\)");
-    return false;
+    std::regex nameRegex("([[:w:]]+)\\s*");
+    std::smatch matches;
+    if (!std::regex_match(line, matches, nameRegex))
+    {
+        return false;
+    }
+    for(size_t i = 0; i < matches.size(); ++i)
+    {
+        std::cout << "match " << i << ": " << matches[i].str() << std::endl;
+    }
+    return true;
 }
 
 bool SceneLoader::loadColorFromString (const std::string& line, Color &color)
 {
-    std::regex colorRegex("(-?\\d+\\.?\\d*),(-?\\d+\\.?\\d*),(-?\\d+\\.?\\d*)");
-
-    return false;
+    std::regex colorRegex("\\((-?\\d+\\.?\\d*),(-?\\d+\\.?\\d*),(-?\\d+\\.?\\d*)\\)");
+    std::smatch matches;
+    if (!std::regex_match(line, matches, colorRegex))
+    {
+        return false;
+    }
+    for(size_t i = 0; i < matches.size(); ++i)
+    {
+        std::cout << "match " << i << ": " << matches[i].str() << std::endl;
+    }
+    return true;
 }
 
 bool loadVectorFromString    (const std::string& line, Vector3f &point)
 {
-    std::regex vectorRegex("(-?\\d+\\.?\\d*),(-?\\d+\\.?\\d*),(-?\\d+\\.?\\d*)");
-
-    return false;
+    std::regex vectorRegex("\\((-?\\d+\\.?\\d*),(-?\\d+\\.?\\d*),(-?\\d+\\.?\\d*)\\)");
+    std::smatch matches;
+    if (!std::regex_match(line, matches, vectorRegex))
+    {
+        return false;
+    }
+    for(size_t i = 0; i < matches.size(); ++i)
+    {
+        std::cout << "match " << i << ": " << matches[i].str() << std::endl;
+    }
+    return true;
 }
--- Scenes/SceneLoader.h
+++ Scenes/SceneLoader.h
@@ -9,12 +9,9 @@ class SceneLoader
         static bool loadSceneFromFile(const std::string& fileName, Scene &scene);
 
     private:
-        static bool loadCameraFromString    (const std::string& line, Scene &scene);
-        static bool loadMaterialFromString  (const std::string& line, Scene &scene);
-        static bool loadLightFromString     (const std::string& line, Scene &scene);
-        static bool loadSphereFromString    (const std::string& line, Scene &scene);
         static bool loadColorFromString     (const std::string& line, Color &color);
         static bool loadVectorFromString    (const std::string& line, Vector3f &point);
+        static bool loadNameFromString      (const std::string& line, std::string &name);
 };
 
 #endif // SCENELOADER_H
