27a28570786332a686bb56641b0a137a4f4a01fa
Benoît Ruiz <bruizy@gmail.com>
2017-10-26 21:06:15 +0200

    Update main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 
 #include "Scenes/Scene.h"
+#include "Scenes/BasicScene.h"
 #include "Scenes/SceneLoader.h"
+
+#include "Utils/Texture.h"
 
 int main()
 {
-    Scene scene;
-    if (SceneLoader::loadSceneFromFile("sample_scene.txt", scene))
-    {
-        scene.render("testPerformance");
-    }
-    else
-    {
-        std::cout << "unable to load scene" << std::endl;
-    }
+
+    BasicScene scene;
+    scene.render("testWithoutTexture");
+//
+//    if (SceneLoader::loadSceneFromFile("sample_scene.txt", scene))
+//    {
+//        scene.render("testPerformance");
+//    }
+//    else
+//    {
+//        std::cout << "unable to load scene" << std::endl;
+//    }
     return 0;
 }
